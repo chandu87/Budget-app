015c8072ee85994b99449aad981596f1d50e9828
chandu <chandrasr.palle@gmail.com>
2018-04-23 12:22:36 +0200

    examples deleted form app js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 var budgetController = (function(){
-    var x = 23;
-    var add = function(a){
-        return x+a;
-    }
     return {
-        publicTest : function(b){
-                    return add(b);
-        }
     }
 })();
 var UIController = (function(){
@@ -14,11 +7,7 @@ var UIController = (function(){
 })();
 
 var controller = (function(budgetCtrl,UICtrl){
-    // var x = budgetCtrl.publicTest(5);
     return {
-        publicController : function(x){
-            console.log(budgetCtrl.publicTest(x));
-        }
     }
 
 })(budgetController,UIController);

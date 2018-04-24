f4322580eff40f3cd2259079c5df374750d85ac9
chandu <chandrasr.palle@gmail.com>
2018-04-24 10:05:44 +0200

    Inputs added in UI controller

--- app.js
+++ app.js
@@ -4,22 +4,44 @@ var budgetController = (function(){
 })();
 var UIController = (function(){
     //UI controller
+    let DOMStrings = {
+            inputType: '.add__type',
+            inputDescription : '.add__description',
+            inputValue : '.add__value',
+            inputButton : '.add__btn'
+    };
+    return {
+        getInput : function(){
+            return {
+                type :document.querySelector(DOMStrings.inputType).value,
+                description : document.querySelector(DOMStrings.inputDescription).value,
+                value : document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+        getDOMStrings : function(){
+            return DOMStrings;
+        }
+    }
+
 })();
 
 var controller = (function(budgetCtrl,UICtrl){
-
+    let DOMStrs = UICtrl.getDOMStrings();
     ctrlAddItem = function(event){
         // 1. add field input data
+        let inputValues = UICtrl.getInput();
+        console.log(inputValues);
+
         // 2. add item to budget controller
         //3. add item to UI
         //4. caluclate budget
         //5. update UI
 
     };
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOMStrs.inputButton).addEventListener('click',ctrlAddItem);
 
     document.addEventListener("keypress",function(event){
-        if(event.keyCode === 97 || event.which === 97){
+        if(event.keyCode === 13 || event.which === 13){
                 ctrlAddItem();
         }
     });

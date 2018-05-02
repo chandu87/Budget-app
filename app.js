9234e83f63fcc85e369ba53137ff15f942b8285a
chandu <chandrasr.palle@gmail.com>
2018-05-02 23:58:02 +0200

    IDs added for all items

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var budgetController = (function() {
-  var Expense = function(id, description, value){
+  var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
   return {
-
+    addItem: function(type, des, val){
+      var newItem, ID;
+      ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+      if(type === 'exp'){
+        newItem = new Expense(ID, des, val);
+      }else if(type === 'inc'){
+        newItem = new Income(ID, des, val)
+      }
+      data.allItems[type].push(newItem);
+      return newItem;
+    }
   };
 })();
 var UIController = (function() {
